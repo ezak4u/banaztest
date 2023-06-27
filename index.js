643f8d1841f342e97f3096b606cd6346b594387e
ezak4u <ezak4u@gmail.com>
2023-06-27 18:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 const port = process.env.PORT || 8080
+var NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 //var FCM = require('fcm-push');
 
 //var serverKey = 'AAAAx2cVVfc:APA91bEqmqbEZwswltWBPCg2F-J0QiN7ALRm31xAEzecuQ8l_RUADb6BSHY2fKpvaxciErOyI73yiBIuF3vcH_YyfI7IZlNDltzUI_9pFQ8gmrBXlqqysPo6tEOjLkXBwUcvEvJ_QPJQ';
@@ -17,10 +19,36 @@ app.post('/bandyer', (req, res) => {
 	var headers  = req.headers;
 	var roomIdHmac  = req.headers["room-id-hmac"];
 	var bandyerSignature  = req.headers["bandyer-signature"];
+	const success = myCache.mset([
+		{key: "myKey1", val: bodyReq,  ttl: 60000},
+		{key: "myKey2", val: roomIdHmac, ttl: 60000},
+		{key: "myKey3", val: bandyerSignature, ttl: 60000},
+	]);
+	console.log("Request ...myCache status ==",success);
 	console.log("Request ...myCache status ==",JSON.stringify(bodyReq));
         console.log("Request ...Body = ",bodyReq);	
         console.log("Request ...roomIdHmac= ",roomIdHmac);
 	console.log("Request ...bandyerSignature= ",bandyerSignature);	
+	setTimeout(function() {
+		console.log("Waiting......");	
+    }, 60*1000);
+    res.send(retRes);	  
+});
+
+app.post('/getcache', (req, res) => {
+    var retRes= myCache.get( "myKey1" );
+	res.setHeader("room-id-hmac", myCache.get( "myKey2" ));
+	res.setHeader("bandyer-signature", myCache.get( "myKey3" ));	
+    res.send(retRes);	  
+});
+
+app.post('/clearcache', (req, res) => {
+    const success = myCache.mset([
+		{key: "myKey1", val: ""},
+		{key: "myKey2", val: ""},
+		{key: "myKey3", val: ""},
+	]);	
+	var retRes= {"Status" :success};
     res.send(retRes);	  
 });
 
